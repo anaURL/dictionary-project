7cdc2d19f22532c11c2aafcba91a4edb87abd089
bananakodira <ana.urlic@gmail.com>
2022-01-06 15:49:31 +0100

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,59 +1,38 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary () { 
     const [word,setWord] = useState("");
-    const [loaded,setLoaded]=useState(false);
+    const [results,setResults]=useState(null);
 
-    function displayWord (response) {
-        setLoaded (true);
-        setWord({ 
-            word: response.data.word,
-            phonetic: response.data.phonetic,
-            pronounce: response.data.phonetics[1].audio,
-            meaning: response.data.meanings[0].partOfSpeach,
-            definition: response.data.meanings.definitions[0],
-
-        })
+    function handleResponse (response) {
+        setResults (response.data[0]);
     }
+         
     function search (event) {
         event.preventDefault();
+
         // documentation: https://dictionaryapi.dev/
          let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-         axios.get(url).then(displayWord);
+         axios.get(url).then(handleResponse);
         }
 
     function handleWordChange (event) {
         setWord(event.target.value);
     }
 
-let form = ( 
-         <div className="Dictionary"> 
-            <form onSubmit={search}> 
-                 <label> 
-         <input type="search" className="search" autoFocus={true}
-        onChange={handleWordChange}/> 
-                </label>
-         <input type="submit" value="Search"/>
+        return ( 
+            <div className="Dictionary"> 
+            <form onSubmit={search}>
+         <input type="search" className="search" autoFocus={true} onChange={handleWordChange}/> 
             </form>
-        </div> );
+            <Results results={results}/>
+            </div> 
+             );
 
-if (loaded) { 
-        return (  
-    <div> 
-        {form}
-        <ul className="results"> 
-            <li>Word: {word.Word} </li>
-            <li>Phonetic: {word.Phonetic} </li>
-            <li>Meaning: {word.Meaning} </li>
-            
-        </ul>
-    </div>
-    );
-}   else {
-    return form;
 }
-}
+
 
 
